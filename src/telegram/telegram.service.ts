fefa8921e91ff6fec415a801ac7ad6a634ea7ef8
w31loa <alexslava.barannik@gmail.com>
2023-10-15 22:24:23 +0500

    начал разиьраться с фичами телеграма

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,7 +1,7 @@
 import { ConfigService } from '@nestjs/config';
-import { Ctx, Start, Update ,On ,Message} from "nestjs-telegraf";
+import { Ctx, Start, Update ,On ,Message, Action, Hears} from "nestjs-telegraf";
 import { MarksService } from 'src/marks/marks.service';
-import {Scenes, Telegraf} from 'telegraf'
+import {Markup, Scenes, Telegraf} from 'telegraf'
 
 type Context = Scenes.SceneContext
 
@@ -17,18 +17,48 @@ export class TelegramService extends Telegraf<Context>{
     onStart(@Ctx() ctx:Context){ 
          ctx.replyWithHTML(`<b>Привет, ${ctx.from.first_name}</b>`)
     }
+    @On('sticker')
+    onStiker(@Message('stiker') message,@Ctx() ctx:Context){
+      ctx.replyWithHTML(`              Выберите предмет             `, Markup.inlineKeyboard([
+          Markup.button.callback("Русский", 'lesson1'),
+          Markup.button.callback('Математика', 'lesson2'),
+          Markup.button.callback('МДК', 'lesson3'),
+          Markup.button.callback('Англиский', 'lesson4'),
+        ])
+      )
+    }
 
-    @On('text')
-    async onMessage(@Message('text') message : string, @Ctx() ctx:Context){
-        // const marks = await 
+    @Hears('/key')
+    async onHears(@Ctx() ctx: Context) {
+      ctx.replyWithHTML('Привет', Markup.keyboard([
+        ['Логин', 'Перезагрузить список оценок'],
+        ['Выход']
+    ]).resize() ) 
+    }
+
+    @Action('lesson4')
+    async onAction(@Ctx() ctx:Context){
       this.marksService.generateResponse({'username': "BARANNIKAP" , 'userpass': `1dbf71922928df9ce9e0e8303bb2c84f02184f8e`}).then(data=>{
-        console.log(data['Иностранный язык'][1].date)
+        console.log(data['Иностранный язык'])
 
-        const replyStr:string = `${data['Иностранный язык'][1].date}: ${data['Иностранный язык'][1].mark}`
+        const replyStr:string = `${data['Иностранный язык'][13].date}: ${data['Иностранный язык'][13].mark}`
         ctx.replyWithHTML(replyStr)
+        
       })
+    }
+
+    // @On('text')
+    // async onMessage(@Message('text') message : string, @Ctx() ctx:Context){
+    //     // const marks = await 
+    //   this.marksService.generateResponse({'username': "BARANNIKAP" , 'userpass': `1dbf71922928df9ce9e0e8303bb2c84f02184f8e`}).then(data=>{
+    //     console.log(data['Иностранный язык'])
+
+    //     const replyStr:string = `${data['Иностранный язык'][13].date}: ${data['Иностранный язык'][13].mark}`
+    //     ctx.replyWithHTML(replyStr)
+        
+    //   })
         // this.marksService.test().then(data=>{
         //     console.log(data)
         // })
-    }
+    // }
 }
